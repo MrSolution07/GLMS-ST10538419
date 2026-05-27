cb2d62b91c8b696c6660089e689e6e02b885ec48
Christian Emungu Bulabula <st10538419@rosebank.edu>
2026-05-27 22:45:56 +0200

    fixes

--- GLMS.Web/ViewModels/ServiceRequestCreateViewModel.cs
+++ GLMS.Web/ViewModels/ServiceRequestCreateViewModel.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GLMS.Web.ViewModels;
@@ -18,9 +19,11 @@ public class ServiceRequestCreateViewModel
     [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be positive.")]
     public decimal CostUsd { get; set; }
 
+    [BindNever]
     [Display(Name = "Estimated ZAR (auto-calculated)")]
     public decimal CostZar { get; set; }
 
+    [BindNever]
     [Display(Name = "Live USD→ZAR Rate")]
     public decimal ExchangeRate { get; set; }
 
